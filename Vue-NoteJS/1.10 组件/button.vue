610519b7cc8ab41faeb6bbb307d7ad65d294cf3c
Evllis <7284395@qq.com>
2017-03-29 19:00:22 +0800

    add file

---
+++ Vue-NoteJS/1.10 组件/button.vue
@@ -0,0 +1,30 @@
+<script>
+    export default {
+        name: 'base-button',
+        props: [],
+        render( createElement ) {
+            return createElement(
+                'button',
+                {
+                    class: ['base-button']
+                },
+                this.$slots.default
+            )
+        },
+        methods: {}
+    }
+</script>
+<style>
+    button.base-button {
+        display: inline-block;
+        padding: 0.5em 1em;
+        cursor: pointer;
+        box-sizing: border-box;
+        border: 1px solid black;
+        color: black;
+        -webkit-border-radius: 4px;
+        -moz-border-radius: 4px;
+        border-radius: 4px;
+        background-color: white;
+    }
+</style>
